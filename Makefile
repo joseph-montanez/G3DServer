25bf2b00831e0639566d81816aee8ed6446ec65d
Joseph Montanez <jmontanez@gorilla3d.com>
2010-07-17 21:15:53 -0700

    finally licensed and officially and installable library!

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-FLAGS = -c -g -Wall
+FLAGS = -c -g -Wall -fPIC
 
-all: libwebserver
+all: libg3dserver.so
 
 clean:
 	find . -type f -name "*.so" -exec rm -f {} \;	
@@ -18,7 +18,7 @@ src/WebBoundary.o: src/WebBoundary.cpp src/WebBoundary.h
 	g++ $(FLAGS) src/WebBoundary.cpp -o src/WebBoundary.o
 	
 src/WebController.o: src/WebController.cpp src/WebController.h src/WebResponse.h src/WebSession.h
-	g++ $(FLAGS) src/WebController.cpp -o src/WebController.o
+	g++ $(FLAGS) -ldl src/WebController.cpp -o src/WebController.o
 	
 src/WebRequest.o: src/WebRequest.cpp src/WebRequest.h src/WebString.h
 	g++ $(FLAGS) src/WebRequest.cpp -o src/WebRequest.o
@@ -35,7 +35,36 @@ src/WebSession.o: src/WebSession.cpp src/WebSession.h src/WebRequest.h src/WebRe
 src/WebString.o: src/WebString.cpp src/WebString.h
 	g++ $(FLAGS) src/WebString.cpp -o src/WebString.o
 
-libwebserver:  src/ServerSocket.o src/Socket.o src/WebBoundary.o src/WebController.o src/WebRequest.o src/WebResponse.o src/WebServer.o src/WebSession.o src/WebString.o
-	ar rcs libwebserver.a src/ServerSocket.o src/Socket.o src/SqlQuery.o src/SqlRow.o src/WebBoundary.o src/WebController.o src/WebRequest.o src/WebResponse.o src/WebServer.o src/WebSession.o src/WebString.o
-    
+libg3dserver.so:  src/ServerSocket.o src/Socket.o src/WebBoundary.o src/WebController.o src/WebRequest.o src/WebResponse.o src/WebServer.o src/WebSession.o src/WebString.o
+	g++ -ldl -shared -Wl,-soname,libg3dserver.so.1 -o libg3dserver.so.1.0 src/ServerSocket.o src/Socket.o src/WebBoundary.o src/WebController.o src/WebRequest.o src/WebResponse.o src/WebServer.o src/WebSession.o src/WebString.o
+
+install:
+	mkdir -p /usr/include/g3dserver
+	cp src/ServerSocket.cpp /usr/include/g3dserver
+	cp src/ServerSocket.h /usr/include/g3dserver
+	cp src/Socket.cpp /usr/include/g3dserver
+	cp src/SocketException.h /usr/include/g3dserver
+	cp src/Socket.h /usr/include/g3dserver
+	cp src/WebBoundary.cpp /usr/include/g3dserver
+	cp src/WebBoundary.h /usr/include/g3dserver
+	cp src/WebController.cpp /usr/include/g3dserver
+	cp src/WebController.h /usr/include/g3dserver
+	cp src/WebCookie.cpp /usr/include/g3dserver
+	cp src/WebCookie.h /usr/include/g3dserver
+	cp src/WebRequest.cpp /usr/include/g3dserver
+	cp src/WebRequest.h /usr/include/g3dserver
+	cp src/WebResponse.cpp /usr/include/g3dserver
+	cp src/WebResponse.h /usr/include/g3dserver
+	cp src/WebServer.cpp /usr/include/g3dserver
+	cp src/WebServer.h /usr/include/g3dserver
+	cp src/WebSession.cpp /usr/include/g3dserver
+	cp src/WebSession.h /usr/include/g3dserver
+	cp src/WebString.cpp /usr/include/g3dserver
+	cp src/WebString.h /usr/include/g3dserver
+	cp libg3dserver.so.1.0 /usr/lib
+	rm -f /usr/lib/libg3dserver.so
+	rm -f /usr/lib/libg3dserver.so.1
+	ln /usr/lib/libg3dserver.so.1.0 /usr/lib/libg3dserver.so
+	ln /usr/lib/libg3dserver.so.1.0 /usr/lib/libg3dserver.so.1
+	
 
--- examples/hello-world/main.cpp
+++ examples/hello-world/main.cpp
@@ -1,21 +1,14 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * main.cc
- * Copyright (C) joseph montanez 2009 <joseph@>
- *
- * main.cc is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * main.cc is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+Copyright (c) 2009-2010, Joseph Montanez
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the Joseph Montanez nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <string>
 #include <stdio.h>
@@ -25,20 +18,15 @@
 /*
  * Web Server
  */
-#include "../webserver/WebServer.h"
+#include "g3dserver/WebServer.h"
+#include "g3dserver/WebController.h"
 
-/*
- * Controllers
- */
-#include "index.cpp"
-#include "lucene.cpp"
-#include "admin/index.cpp"
-#include "admin/login.cpp"
-#include "admin/logout.cpp"
-#include "admin/dashboard.cpp"
-#include "admin/blog/posts.cpp"
-#include "admin/blog/comments.cpp"
-#include "admin/upload/form.cpp"
+class IndexController : public WebController {
+public: 
+	void get() {
+		this->response->body.append("Hello World!");
+	}
+};
 
 int main(int argc, char* argv[]) {
 
@@ -46,15 +34,6 @@ int main(int argc, char* argv[]) {
 
     server->port = 8181;
     server->addController("/", new IndexController());
-    server->addController("/test", new WebController("/home/joseph/NetBeansProjects/FireController/dist/Debug/GNU-Linux-x86/libFireController.so"));
-    server->addController("/lucene", new LuceneController());
-    server->addController("/admin", new admin::IndexController());
-    server->addController("/admin/login", new admin::LoginController());
-    server->addController("/admin/logout", new admin::LogoutController());
-    server->addController("/admin/dashboard", new admin::DashboardController());
-    server->addController("/admin/blog-posts", new admin::blog::PostController());
-    server->addController("/admin/blog-comments", new admin::blog::CommentController());
-    server->addController("/admin/upload-example", new admin::upload::FormController());
 
     server->run();
     delete server;
